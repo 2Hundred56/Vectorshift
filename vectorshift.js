c6df1586b7b9a0dad72eefc2f910246f987a8801
iahuang <ianhuang46@gmail.com>
2017-12-25 22:36:14 -0500

    Bezier code

--- vectorshift.js
+++ vectorshift.js
@@ -4,10 +4,44 @@ class Vector {
         this.y = y;
     }
 }
+class Node {
+    constructor (x,y) {
+        this.pos = new Vector(x,y);
+
+    }
+}
+function factorial (n) {
+  if (n == 0 || n == 1)
+    return 1;
+  if (f[n] > 0)
+    return f[n];
+  return f[n] = factorial(n-1) * n;
+} ​
 class Path {
+    function init () {
+        this.nodes = []
+    }
+    // https://en.wikipedia.org/wiki/Bezier_curve
+    function bezierCoord(points,coord,t) {
+        var n = points.length-1;
+        var sum = 0;
+        for (var i=0;i<=n;i++) {
+            var ni = (factorial(n))/(factorial(n-i)*factorial(i));
+            var value = ni*pow(1-t,n-i)*pow(t,i)*points[i][coord];
+            sum+=value;
+        }
+        return sum
+    }
+    function bezier(points,t) {
+        return Vector(
+            bezierCoord(points,t,0),
+            bezierCoord(points,t,1)
+        );
+    }
     constructior () {
 
     }
+
 }
 var canvasSize;
 var processing;
